fce45c874726763e0430cec58bae1ada24518fc3
Egor Vinogradov <egor@ostrovok.ru>
2012-06-23 04:07:27 +0400

    ostrovok.ru prices on booking.com

--- common.js
+++ common.js
@@ -1,75 +1,67 @@
-var Config = {
-    booking: {
-        url: /booking\.com/,
-        js: 'services/booking.js',
-        css: 'services/booking.css'
-    },
-    expedia: {
-        url: /expedia\.com|expedia\.co\.uk/,
-        js: 'services/expedia.js',
-        css: 'services/expedia.css'
-    },
-    oktogo: {
-        url: /oktogo\.ru/,
-        js: 'services/oktogo.js',
-        css: 'services/oktogo.css'
-    }
-};
-
-
 var PriceAlert = {
 
-    init: function(){
-
-        var onTabChanged = function(){
-            this.getCurrentUrl(function(url){
-                var OTA = this.getCurrentOTA(url);
-                if ( OTA ) {
-                    this.insertJS(OTA.js);
-                    this.insertCSS(OTA.css);
+    init: function(config){
+        
+        var onTabChanged = function(url, tabId, options){
+            var OTA = this.getCurrentOTA(url, config);
+            if ( OTA ) {
+                if ( options.newTab ) {
+                    this.insertJS(tabId, OTA.js);
+                    this.insertCSS(tabId, OTA.css);
                 }
-            });
+                this.setIcon({ active: true });
+            }
+            else {
+                this.setIcon({ active: false });
+            }
         };
 
         this.tabChanged(onTabChanged);
-
+        this.tabUpdated(onTabChanged);
     },
-    tabChanged: function(callback){
-        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tabInfo){
+    tabUpdated: function(callback){
+        var _this = this;
+        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
             if ( changeInfo.status === 'complete' ) {
-                callback(tabInfo.url, tabId);
+                console.log('updated:', tab.url, tabId, changeInfo, tab);
+                callback.call(_this, tab.url, tabId, { newTab: true });
             }
         });
     },
-    getCurrentUrl: function(callback){
-        chrome.windows.getCurrent(function(w){
-            chrome.tabs.query({ windowId: w.id, active: true }, function(arrayOfOneTab){
-                callback(arrayOfOneTab[0].url);
+    tabChanged: function(callback){
+        var _this = this;
+        chrome.tabs.onSelectionChanged.addListener(function(tabId){
+            console.log('changed:', tabId);
+            chrome.windows.getCurrent(function(data){
+                chrome.tabs.query({ windowId: data.id, active: true }, function(tabs){
+                    console.log('-- current:', tabs[0].url, tabs[0].id, tabs);
+                    callback.call(_this, tabs[0].url, tabs[0].id, { newTab: false });
+                });
             });
         });
     },
-    getCurrentOTA: function(url){
-        for ( var service in Config ) {
-            if ( Config[service].url.test(url) ) {
-                return Config[service];
+    getCurrentOTA: function(url, config){
+        for ( var service in config ) {
+            if ( config[service].url.test(url) ) {
+                return config[service];
             }
         }
         return false;
     },
-    insertJS: function(path){
-
-        var tabId = 1; // TODO: get TabId
-
-        chrome.tabs.executeScript(tabId, { file: path }, function(data){
-            console.log('--- JS executed', data);
+    setIcon: function(options){
+        var path = options.active
+            ? 'img/active.png'
+            : 'img/inactive.png';
+        chrome.browserAction.setIcon({ path: path });
+    },
+    insertJS: function(tabId, code){
+        chrome.tabs.executeScript(tabId, { code: code }, function(){
+            console.log('INSERT JS', tabId);
         });
     },
-    insertCSS: function(path){
-
-        var tabId = 1; // TODO: get TabId
-
-        chrome.tabs.insertCSS(tabId, { file: path }, function(data){
-            console.log('--- CSS inserted', data);
+    insertCSS: function(tabId, code){
+        chrome.tabs.insertCSS(tabId, { code: code }, function(){
+            console.log('INSERT CSS', tabId);
         });
     }
 };
--- services/booking.js
+++ services/booking.js
@@ -5,55 +5,156 @@
                 links: [],
                 rooms: [],
                 arrivalDate: booking.env.b_checkin_date,
-                departureDate: booking.env.b_checkout_date
+                departureDate: booking.env.b_checkout_date,
+                callback: 'handleData'
             },
-            url: 'http://ostrovok.ru/api/v1/pricealert',
+            url: 'http://pricealert.f.test.ostrovok.ru/api/v1/pricealert/',
             success: function(data){},
             error: function(data){}
-        };
+        },
+        handleData = function(data){
 
-    $('link[rel="alternate"]').each(function(i, e){
+            console.log('-- handle data', data);
 
-        var element = $(e),
-            url = element.attr('href').split('?')[0];
-        request.data.links.push(url);
-    });
+            if ( data.status !== 'OK' ) {
+                console.log('room data fail', data);
+            }
+
+            var rooms = data.data;
+
+            bookingData.forEach(function(bookingRoom, i){
+
+                rooms.forEach(function(room){
+
+                    room.rooms_data.forEach(function(roomsData){
+
+                        var ostrovokRoom = {},
+                            condition = false;
+
+                        for ( var i = 0, l = roomsData.room.value_adds.length; i < l; i++ ) {
+                            if ( roomsData.room.value_adds[i].code === 'has_meal' ) {
+                                ostrovokRoom.meal = true;
+                                return;
+                            }
+                        }
+
+                        ostrovokRoom.adults = roomsData.adults;
+                        ostrovokRoom.price = roomsData.total_rate;
+                        ostrovokRoom.ratio = roomsData.ratio;
+
+                        ostrovokRoom.free_cancellation = true;
+                        ostrovokRoom.is_postpay = true;
+
+                        // TODO: get cancellation policy
+                        // TODO: get is_postpay
+
+                        condition =
+                            bookingRoom.adults == ostrovokRoom.adults &&
+                            bookingRoom.meal == ostrovokRoom.meal &&
+                            bookingRoom.free_cancellation == ostrovokRoom.free_cancellation &&
+                            ostrovokRoom.ratio >= 0.8;
+
+                        if ( condition ) {
+                            ostrovokData[room.booking_room_id] = {
+                                price: ostrovokRoom.price,
+                                is_postpay: ostrovokRoom.is_postpay
+                            };
+                        }
+
+                    });
+//                    if ( room.match ) {
+//                        // TODO: set price room.booking_room_id
+//                        ostrovokData[room.booking_room_id]
+//                        //isMatches = true;
+//                    }
+//                    else {
+//                    }
+                });
+
+            });
+
+            window.ostrovokData = ostrovokData;
+            console.log('ostrovok data', ostrovokData);
+
+        },
+//            bookingData = [
+//                {
+//                    id: 58669,
+//                    name: 'ergrtjk',
+//                    adults: 2,
+//                    free_meal: true,
+//                    free_cancellation: true
+//                },
+//                {
+//                    id: 58669,
+//                    name: 'ergrtjk',
+//                    adults: 2,
+//                    free_meal: true,
+//                    free_cancellation: true
+//                }
+//            ],
+        bookingData = [],
+        ostrovokData = {};
+
+        $('link[rel="alternate"]').each(function(i, e){
+
+            var element = $(e),
+                url = element.attr('href').split('?')[0];
+            request.data.links.push(url);
+        });
 
 
-    $('[class^="room_loop_counter"]').not('.extendedRow').each(function(i, element){
+        $('[class^="room_loop_counter"]').not('.extendedRow').each(function(i, element){
 
-        var data = {},
-            els = {
-                container: $(element)
-            };
+            var data = {},
+                els = {
+                    container: $(element)
+                };
 
-        els.roomName = els.container.find('.togglelink');
-        els.roomNameWrapper = els.roomName.parent();
+            els.roomName = els.container.find('.togglelink');
+            els.roomNameWrapper = els.roomName.parent();
+            els.policies = els.container.find('.ratepolicy');
+            els.adults = els.container.find('.roomDefaultUse img');
 
-        data.name = els.roomName.html();
-        data.booking_room_id = els.roomNameWrapper.attr('id');
+            data.name = els.roomName.html();
+            data.booking_room_id = els.roomNameWrapper.attr('id');
 
-        if ( data.name && data.booking_room_id ) {
-            request.data.rooms.push(data);
-        }
-    });
+            data.free_meal = els.policies.filter(':contains("БЕСПЛАТНАЯ отмена бронирования")');
+            data.free_cancellation = els.policies.filter(':contains("Завтрак включен")');
+            data.adults = parseFloat(els.adults.attr('class').split(' ')[1].replace(/max/, ''));
+
+            if ( data.name && data.booking_room_id ) {
+                request.data.rooms.push(data);
+                bookingData.push({
+                    id: data.booking_room_id,
+                    name: data.name,
+                    adults: data.adults,
+                    free_meal: !!data.free_meal,
+                    free_cancellation: !!data.free_cancellation
+                });
+            }
+        });
+
+        //bookingData.rooms = request.data.rooms;
 
 
-    request.data.links = JSON.stringify(request.data.links);
-    request.data.rooms = JSON.stringify(request.data.rooms);
+        request.data.links = JSON.stringify(request.data.links);
+        request.data.rooms = JSON.stringify(request.data.rooms);
+
+        $('body').append('<script type="text/javascript" src="' + $.param(request.data) + '"></script>');
 
 
-    //$.ajax({
-    //    type: 'GET',
-    //    url: request.url,
-    //    data: $.param(request.data),
-    //    success: request.success,
-    //    error: request.error
-    //});
+        //$.ajax({
+        //    type: 'GET',
+        //    url: request.url,
+        //    data: $.param(request.data),
+        //    success: request.success,
+        //    error: request.error
+        //});
 
-    console.log('--- DATA:', request, '\n\n', $.param(request.data));
+        console.log('--- DATA:', request, '\n\n', $.param(request.data));
 
-    window.request = request // temporary for debug
+        window.handleData = handleData;
+        window.request = request; // temporary for debug
 
 }());
-
