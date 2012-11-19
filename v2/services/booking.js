193ec5da4da178b9626ff740a7d47e1443ce34bb
Egor Vinogradov <egor@ostrovok.ru>
2012-11-19 13:38:28 +0400

    fixed bugs; removed unused code

--- v2/services/booking.js
+++ v2/services/booking.js
@@ -135,13 +135,6 @@ function getRoomAdults(el){
 
 /* hotel */
 
-// function getHotelDates(){
-//     return {
-//         arrivalDate: booking.env.b_checkin_date,
-//         departureDate: booking.env.b_checkout_date
-//     }
-// };
-
 function getHotelArrivalDate(){
     return booking.env.b_checkin_date;
 };
@@ -162,36 +155,6 @@ function getHotelCountry(){
     return $('a[rel=v:url][href^=/country]').html()
 };
 
-// function getHotelAdults(){
-//     var el = $('.guests .search_summary_toggle_button');
-//     if ( el.length ) {
-//         return +el
-//             .html()
-//             .split(',')[0]
-//             .replace(/[^[0-9]/, '');
-//     }
-//     return 2;
-// };
-
-// function getHotelChildren(){
-//     var el = $('.guests .search_summary_toggle_button');
-//     if ( el.length ) {
-//         var children = el
-//             .html()
-//             .split(',')[1];
-//         if ( children ) {
-//             return +children.replace(/[^[0-9]/, '');
-//         }
-//         else {
-//             return 0;
-//         }
-//     }
-//     else {
-//         return 0;
-//     }
-// };
-
-
 function getHotelAdults(){
     return booking.env.b_group[0].guests;
 };
@@ -206,6 +169,8 @@ function getHotelChildrenAges(){
     });
 };
 
+/* ostrovok */
+
 function getOstrovokHotelId(params){
     $.ajax({
         url: params.config.host + params.config.hotels.url,
@@ -214,11 +179,8 @@ function getOstrovokHotelId(params){
         },
         success: function(data){
             if ( data.hotels.length ) {
-                var hotelId = +data
-                    .hotels[0]
-                    .hotel_uid;
                 params.success && params.success({
-                    hotelId: hotelId
+                    hotelId: data.hotels[0].hotel_uid
                 });
             }
             else {
@@ -256,7 +218,7 @@ function getOstrovokRooms(params){
     }
 
     $.ajax({
-        url: params.config.rooms.urlPrefix + params.hotelId,
+        url: params.config.host + params.config.rooms.urlPrefix + params.hotelId,
         data: requestParams,
         success: function(data){
             if ( data.hotel && data.hotel.rooms && data.hotel.rooms.length ) {
@@ -282,28 +244,21 @@ function getOstrovokRooms(params){
     });
 };
 
-
-
-
-
-
-
 function init(callback){
     var rooms = getRoomData(roomConfig);
-    console.log
     if ( rooms.length ) {
         getOstrovokHotelId({
             config: ostrovokAPIConfig,
             hotel:  getHotelName(),
             city:   getHotelCity(),
             success: function(data){
-                if ( hotelId ) {
+                if ( data.hotelId ) {
                     getOstrovokRooms({
                         config: ostrovokAPIConfig,
                         hotelId: data.hotelId,
                         success: function(data){
-                            if ( data.rooms && data.rooms.length ) {
-                                callback && callback(data.hotel.rooms);
+                            if ( data.rooms.length ) {
+                                callback && callback(data.rooms);
                             }
                         },
                         error: function(e){
@@ -317,53 +272,12 @@ function init(callback){
             }
         });
     }
-    return rooms;
 };
 
+
+
+/**********/
+
 zcallback = function(rooms){
-    console.log('--- ROOMS', rooms);
+    console.log('___ ROOMS', rooms);
 }
-
-
-// http://ostrovok.ru/api/v1/rooms/x863982519/?arrivalDate=2012-11-16&hotelId=863982519&departureDate=2012-11-17&room1_numberOfAdults=2&room1_numberOfChildren=0&_type=json&grouped=true&payment_choices=true
-
-
-
-//var url = 'http://ostrovok.ru/api/v1/rooms/x863982519/';
-var url = 'http://127.0.0.1:5000/api/v1/rooms/x863982519/';
-var data = {
-    arrivalDate: '2012-11-16',
-    departureDate: '2012-11-17',
-    hotelId: 863982519,
-    room1_numberOfAdults: 2,
-    room1_numberOfChildren: 0,
-    _type: 'json',
-    grouped: true,
-    payment_choices: true
-}
-
-function makeRequest(){
-    $.ajax({
-        type: 'GET',
-        dataType: 'jsonp',
-        url: url,
-        data: data,
-        complete: function(){
-            console.log('>>> Complete', arguments);
-        }
-    });
-}
-
-
-
-
-
-function z(s){
-    console.log('{');
-    s.split('&').forEach(function(n){
-        var z = n.split('=');
-        console.log('    ', z[0], ':', z[1], ',');
-    });
-    console.log('}');
-}
-
